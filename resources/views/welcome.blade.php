f4d8975c555685f77f39dedfa9474dcd3cbce409
lfwdev <66641151+lfwdev@users.noreply.github.com>
2021-07-23 18:11:04 -0300

    bootstrap and font-awesome

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('components.layout')
+@section('main')
+    @include('components.sections.hero')
+    @include('components.sections.features')
+    @include('components.sections.about')
+    @include('components.sections.cta')
+@endsection
