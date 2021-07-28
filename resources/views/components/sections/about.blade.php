90bebbdc7d62d762e48abb0a618d13049c31f436
lfwdev <66641151+lfwdev@users.noreply.github.com>
2021-07-28 17:48:20 -0300

    Changes to Readme and about

--- resources/views/components/sections/about.blade.php
+++ resources/views/components/sections/about.blade.php
@@ -4,13 +4,6 @@
             <div class="col-md-8 text-center" >
                 <h2>About</h2>
                 <div class="divider"></div>
-                <a class="btn btn-outline-secondary" href="https://github.com/lfwdev/moatmusic">
-                    <div class="p-1">
-                        <span> Check this out on github</span>
-                        &nbsp;&nbsp;
-                        <em class="fab fa-github"></em>
-                    </div>
-                </a>
             </div>
         </div>
         <div class="row">
@@ -18,6 +11,15 @@
                 <div> <em class="fa fa-readme"></em></div>
                 {{ Illuminate\Mail\Markdown::parse(file_get_contents(base_path() . '/README.md')) }}
             </div>
+        </div>
+        <div class="row justify-center pt-5 pb-5">
+            <a class="btn btn-outline-primary" href="https://github.com/lfwdev/moatmusic" target="_blank">
+                <div class="p-1">
+                    <span> Check this out on github</span>
+                    &nbsp;&nbsp;
+                    <em class="fab fa-github"></em>
+                </div>
+            </a>
         </div>
     </div>
 </section>
